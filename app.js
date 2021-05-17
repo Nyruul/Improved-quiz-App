3fe755fa69a9a86545f5bfee56328c22a63e245e
viralz <nyarlatet@gmail.com>
2021-05-16 19:08:50 -0700

    forgot to save js

---
+++ app.js
@@ -0,0 +1,51 @@
+/**I am basically rewriting my original quiz app, only after hardcoding it a second time
+ * I will be improving them with new skills aquired, and even more over time.
+*/
+
+const store = {
+    //I want to create a varying selection of questions this time.
+    questions:[
+        {
+            question:'This is a question?',
+            answers:[
+                'one',
+                'two',
+                'three',
+                'four'
+            ],
+            correctAnswer: 'correct'
+        },
+        {
+            question: 'Why more questions?',
+            answers:[
+                'uno',
+                'two',
+                'Eh',
+                'you\'re feelin\'in it!'
+            ],
+            correctAnswer: 'Tbh I was feel\'in it!'
+        },
+        {
+            question: 'Why more questions?',
+            answers:[
+                'uno',
+                'two',
+                'Eh',
+                'you\'re feelin\'in it!'
+            ],
+            correctAnswer: 'Tbh I was feel\'in it!'
+        },
+        {
+            question: 'Why more questions?',
+            answers:[
+                'uno',
+                'two',
+                'Eh',
+                'you\'re feelin\'in it!'
+            ],
+            correctAnswer: 'Tbh I was feel\'in it!'
+        }
+
+
+    ]
+}
